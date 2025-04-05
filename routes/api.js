b90c8e4bb4ad37769e8561582a66357adbdd20d7
Mikulas Machovec <mikimachovec@gmail.com>
2025-04-05 10:06:37 +0000

    helmet and get query

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,27 @@
 'use strict';
 
+const { response } = require("express");
+
 module.exports = function (app) {
 
   app.route('/api/stock-prices')
-    .get(function (req, res){
-      
+    .get(async (req, res) =>{
+      let { stock, like} = req.query
+      stock = stock.toUpperCase()
+      try{
+        const result = await fetch(`https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${stock}/quote`)
+        
+        if(!result.ok){
+          throw new Error('Failed to fetch stock data');
+        }
+        const stockData = await result.json()
+
+        console.log(stockData)
+      }
+      catch(error){
+        console.error('Error fetching stock data:', error);
+      }
     });
+
     
 };
